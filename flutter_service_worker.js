80ba0ac8892d172aec853b4faf3c2d04a74a533a
jiyuujin <jiyuujin@users.noreply.github.com>
2022-04-06 23:03:48 +0000

    deploy: 4e41e3d7d62543c871ade73048518a5cfca9656f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"index.html": "f95291f800b26001e4a2ffecdf37d79a",
-"/": "f95291f800b26001e4a2ffecdf37d79a",
+"index.html": "ad874ab47396ce4787fcb04604997d2d",
+"/": "ad874ab47396ce4787fcb04604997d2d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "main.dart.js": "51173ebc63b3ceb2fb43921bb7b0fc20"
